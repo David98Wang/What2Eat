8d4db8d691b60fd53bfcc0b88fab4aa78cf7e66b
David Wang <whcdavidca@gmail.com>
2017-05-27 10:10:33 -0400

    Integrated Result Activity.

--- app/src/main/java/eng/waterloo/what2eat/QRCodeActivity.java
+++ app/src/main/java/eng/waterloo/what2eat/QRCodeActivity.java
@@ -1,14 +1,18 @@
 package eng.waterloo.what2eat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -16,6 +20,7 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.encoder.Encoder;
 
 import java.util.Random;
 
@@ -26,37 +31,22 @@ import static android.R.id.content;
  */
 
 public class QRCodeActivity extends Activity {
+    public static int NumberOfPeople = -1;
+    public static String groupName;
+    ImageView imgQRCode;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
-
+        imgQRCode = (ImageView)findViewById(R.id.imgQRCode);
         addKeyListener();
     }
 
     protected void displayQRCode() {
         Bitmap bmp = encodeToQrCode(randomString(),1000,1000);
 
-        /*Random rand = new Random();
-        ImageView imgQRCode = (ImageView) findViewById(R.id.imgQRCode);
-        LinearLayout layoutMain = (LinearLayout) findViewById(R.id.layoutMain);
-        int length = 600;
-        Log.d("DEBUG", String.valueOf(length));
-        Log.d("DEBUG", String.valueOf(length));
-        int QRSize = 50;
-        int blockPixel = length / QRSize - 1;
-        Bitmap bmp = Bitmap.createBitmap(600, 600, Bitmap.Config.RGB_565);
-        for (int x = 0; x < length - 100; x += blockPixel) {
-            for (int y = 0; y < length - 100; y += blockPixel) {
-                boolean res = (rand.nextBoolean());
-                for (int a = 0; a < blockPixel; a++)
-                    for (int b = 0; b < blockPixel; b++)
-                        bmp.setPixel(x + a, y + b, res ? Color.BLACK : Color.WHITE);
-            }
-        }*/
         ((ImageView) findViewById(R.id.imgQRCode)).setImageBitmap(bmp);
 
     }
-
     protected void addKeyListener() {
         findViewById(R.id.btnJoinGroup).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -75,28 +65,69 @@ public class QRCodeActivity extends Activity {
         });
         findViewById(R.id.btnNewGroup).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                displayQRCode();
+                popUp();
+                new Thread(new Runnable() {
+                    public void run() {
+                        // a potentially  time consuming task
+                        QRCodeActivity.groupName=randomString();
+                        final Bitmap temp = encodeToQrCode(groupName,1000,1000);
+                        imgQRCode.post(new Runnable() {
+                            public void run() {
+                                imgQRCode.setImageBitmap(temp);
+                            }
+                        });
+                    }
+                }).start();
+                //displayQRCode();
             }
         });
+        findViewById(R.id.btnFinish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(QRCodeActivity.this,ResultActivity.class);//Change to real activity
+                startActivity(next);
+            }
+        });
+    }
+    public void popUp(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Number of People");
+        alert.setMessage("Please enter number of people in your Group");
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        alert.setView(input);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                if(input.getText().toString()=="")input.setText("1");
+                QRCodeActivity.NumberOfPeople =  Math.max(Integer.valueOf(input.getText().toString()),1);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
     }
     public static Bitmap encodeToQrCode(String text, int width, int height){
         QRCodeWriter writer = new QRCodeWriter();
         BitMatrix matrix = null;
+
         try {
-            matrix = writer.encode(text, BarcodeFormat.QR_CODE, 100, 100);
+            matrix = writer.encode(text, BarcodeFormat.QR_CODE, 1000, 1000);
         } catch (WriterException ex) {
             ex.printStackTrace();
         }
-        int[][] pix  = {{0,0,0,0,0,0,0,0,0,0},{1,1,1,1,1,1,1,1,1,1}};
         Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-        //Log.d("ERROR123",String.valueOf(bmp.getWidth()));
-        for (int x = 0; x < 100; x++){
-            for (int y = 0; y < 100; y++){
-                for(int a =0;a<10;a++)
-                    for(int b=0;b<10;b++) {
-                        Log.d("ERROR123",String.valueOf(y*10+b));
-                        bmp.setPixel(x * 10 + a, y * 10 + b, matrix.get(x, y) ? Color.BLACK : Color.WHITE);
-                    }
+        Log.d("ERROR123",String.valueOf(bmp.getWidth()));
+        for (int x = 0; x < 1000; x++){
+            for (int y = 0; y < 1000; y++){
+                        bmp.setPixel(x , y , matrix.get(x, y) ? Color.BLACK : Color.WHITE);
             }
         }
         return bmp;
