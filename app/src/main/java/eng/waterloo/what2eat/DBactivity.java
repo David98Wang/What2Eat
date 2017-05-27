168f0b5a883010e47a19f73f44f9a2e9a235cd9e
f2280c <f2280c@gmail.com>
2017-05-27 12:11:19 -0400

    can listen to db activity everytime data is modified

--- app/src/main/java/eng/waterloo/what2eat/DBactivity.java
+++ app/src/main/java/eng/waterloo/what2eat/DBactivity.java
@@ -9,6 +9,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.security.acl.Group;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static android.R.attr.data;
 import static android.R.attr.key;
 
 
@@ -33,62 +35,83 @@ public class DBactivity extends AppCompatActivity {
     HashMap<String, String> map = new LinkedHashMap<>();
 
     String largestVotingRestaurant;
+
+    DatabaseReference a = root.child("restaurants");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dbactivity);
         //  addToDB("kenzo");
         getHighest();
+        test();
 
+    }
+
+    public void test() {
+        root.child("restaurants").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                /* everytime the data is changed, this will run */
+                System.out.println("data is changed");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void getHighest() {
 
         root.child("restaurants").addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        //get map of users in datasnapshot
+                                                                     @Override
+                                                                     public void onDataChange(DataSnapshot dataSnapshot) {
 
-                        for (DataSnapshot d : dataSnapshot.getChildren()) {
-                            map.put(d.getKey().toString(),d.getValue().toString());
-                        }
-                        //output();
-                        /* put ratings in an array and find out the biggest rating */
-                        int[] totalEle = new int[map.size()];
-                        int i=0;
-                        for (String c : map.keySet()) {
-                            totalEle[i] = Integer.parseInt(map.get(c));
-                            i++;
-                        }
-                        Arrays.sort(totalEle);
-                        int biggest = totalEle[map.size()-1];
+                                                                         for (DataSnapshot d : dataSnapshot.getChildren()) {
+                                                                             map.put(d.getKey().toString(), d.getValue().toString());
+                                                                         }
 
-                        for (String c : map.keySet()){
-                            if(Integer.parseInt(map.get(c))==biggest){
-                                largestVotingRestaurant = c;
-                                break;
-                            }
+                                                                         int[] totalEle = new int[map.size()];
+                                                                         int i = 0;
+                                                                         for (String c : map.keySet()) {
+                                                                             totalEle[i] = Integer.parseInt(map.get(c));
+                                                                             i++;
+                                                                         }
+                                                                         Arrays.sort(totalEle);
+                                                                         int biggest = totalEle[map.size() - 1];
 
-                        }
+                                                                         for (String c : map.keySet()) {
+                                                                             if (Integer.parseInt(map.get(c)) == biggest) {
+                                                                                 largestVotingRestaurant = c;
+                                                                                 break;
+                                                                             }
 
-                    }
+                                                                         }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
+                                                                         output();
 
-                    }
-                }
+                                                                     }
+
+                                                                     @Override
+                                                                     public void onCancelled(DatabaseError databaseError) {
+                                                                         System.out.println(databaseError.getMessage());
+                                                                     }
+                                                                 }
         );
 
 
     }
 
-public void output(){
-    for (String c : map.keySet()){
-        System.out.println("this is map: " + map.get(c));
-        System.out.println("this is key: " + c);
+    public void output() {
+        for (String c : map.keySet()) {
+            System.out.println("this is map: " + map.get(c));
+            System.out.println("this is key: " + c);
+            System.out.println("final string is: " + largestVotingRestaurant);
+        }
     }
-}
+
     public void addToDB(final String restaurant) {
 
         root.child("restaurants").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -112,7 +135,7 @@ public void output(){
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                System.out.println(databaseError.getMessage());
             }
         });
     }
