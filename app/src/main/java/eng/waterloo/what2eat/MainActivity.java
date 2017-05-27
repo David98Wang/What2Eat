81eae815a33b15f28e1869a0ebe788dd33513aca
f2280c <f2280c@gmail.com>
2017-05-26 23:43:14 -0400

    added database activity

--- app/src/main/java/eng/waterloo/what2eat/MainActivity.java
+++ app/src/main/java/eng/waterloo/what2eat/MainActivity.java
@@ -1,13 +1,22 @@
 package eng.waterloo.what2eat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent dbActivity = new Intent(getApplicationContext(), DBactivity.class);
+        startActivity(dbActivity);
+
     }
 }
